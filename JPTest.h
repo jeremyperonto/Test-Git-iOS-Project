ae22561e244d657a091b69396c007a8548137f72
jeremyperonto <jeremy@peronto.com>
2014-09-10 00:41:08 -0700

    added a test class

---
+++ JPTest.h
@@ -0,0 +1,13 @@
+//
+//  JPTest.h
+//  Git Test
+//
+//  Created by Jeremy Peronto on 9/10/14.
+//  Copyright (c) 2014 Jeremy Peronto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPTest : NSObject
+
+@end
---
+++ JPTest.m
@@ -0,0 +1,13 @@
+//
+//  JPTest.m
+//  Git Test
+//
+//  Created by Jeremy Peronto on 9/10/14.
+//  Copyright (c) 2014 Jeremy Peronto. All rights reserved.
+//
+
+#import "JPTest.h"
+
+@implementation JPTest
+
+@end
